8e98a24e1b140569aa3173c0a811948bc6ea976b
Michal Bulla <michal.bulla@gmail.com>
2026-04-01 09:55:23 +0200

    Tweaks

--- source/tailwind.config.js
+++ source/tailwind.config.js
@@ -5,16 +5,17 @@ module.exports = {
   theme: {
     fontFamily: {
       primary: ['ui-sans-serif', 'sans-serif'],
-      //   secondary: ['ui-sans-serif', 'sans-serif'],
+      secondary: ['Montserrat', 'sans-serif'],
     },
     screens: {
       sm: '450px',
+      'sm-md': '600px',
       md: '768px',
       lg: '1024px',
-      xl: '1350px',
+      xl: '1100px',
       // We want the maximum container to stop at 1350px
       // so we set the last breakpoint to the same value
-      '2xl': '1350px',
+      '2xl': '1100px',
       // The original Tailwind 2xl breakpoint value
       // '2xl': '1536px',
     },
@@ -25,12 +26,15 @@ module.exports = {
       colors: {
         brand: {
           // put the custom site color here
-          blue: colors.blue[500],
+          'gray-light': '#F3F3F3',
+          'gray': '#1b1b1b',
+          'green': '#0A6108',
+          'green-light': '#AFB924',
         },
         content: {
           'text': colors.gray[600],
           'link': colors.blue[700],
-          'h2': colors.gray[600],
+          'h2': '#0A6108',
           'h3': colors.gray[600],
           'h4': colors.gray[600],
           'divider': colors.gray[300],
@@ -64,7 +68,7 @@ module.exports = {
         // 'callout-red': "url('/img/callout-graphic_red.svg'), linear-gradient(to right, #E7751F, #D33D4C)",
       },
       boxShadow: {
-        // 'btn' : '2px 2px 0 rgba(0, 0, 0, .5)',
+        'box' : '0px 0px 3px rgba(0, 0, 0, .5)',
       },
     },
   },
